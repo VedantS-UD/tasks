593ad72f35d3372fe29958ca86a1622004bd0835
VedantS-UD <vsub@udel.edu>
2022-02-22 20:16:15 -0500

    Passed Integration Tests for Cycle Holiday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,64 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday =
+    | "🐲" // Chinese New Year
+    | "🎁" // Christmas
+    | "🎃" // Halloween
+    | "🥧" // Pi Day
+    | "🍫"; // World Chocolate Day
+
+// Holiday Dates
+//"🐲" Feb
+//"🥧" Mar 14
+//"🍫" Jul 7
+//"🎃" Oct 31
+//"🎁" Dec 25
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, nextHoliday] = useState<Holiday>("🐲");
+    function alphabet(): void {
+        if (holiday === "🐲") {
+            nextHoliday("🎁");
+        } else if (holiday === "🎁") {
+            nextHoliday("🎃");
+        } else if (holiday === "🎃") {
+            nextHoliday("🥧");
+        } else if (holiday === "🥧") {
+            nextHoliday("🍫");
+        } else {
+            nextHoliday("🐲");
+        }
+    }
+    function date(): void {
+        if (holiday === "🐲") {
+            nextHoliday("🥧");
+        } else if (holiday === "🥧") {
+            nextHoliday("🍫");
+        } else if (holiday === "🍫") {
+            nextHoliday("🎃");
+        } else if (holiday === "🎃") {
+            nextHoliday("🎁");
+        } else {
+            nextHoliday("🐲");
+        }
+    }
+    const alpha = (
+        <div>
+            <Button onClick={alphabet}>Advance by Alphabet</Button>
+        </div>
+    );
+    const year = (
+        <div>
+            <Button onClick={date}>Advance by Year</Button>
+        </div>
+    );
+    const Cycle = (
+        <div>
+            <span>Holiday: {holiday}</span>
+            {alpha}
+            {year}
+        </div>
+    );
+    return Cycle;
 }
