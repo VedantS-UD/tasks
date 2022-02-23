4f4ace0fa43afa48277153be1e8b4a4ff543122c
VedantS-UD <vsub@udel.edu>
2022-02-22 20:15:34 -0500

    Passed Integration Tests for Change Type

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,23 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, flipType] = useState<QuestionType>("short_answer_question");
+    function flip(): void {
+        if (type === "multiple_choice_question") {
+            flipType("short_answer_question");
+        } else {
+            flipType("multiple_choice_question");
+        }
+    }
+    const Change = (
+        <div>
+            <Button onClick={flip}>Change Type</Button>
+            {type === "multiple_choice_question" ? (
+                <div>Multiple Choice</div>
+            ) : (
+                <div>Short Answer</div>
+            )}
+        </div>
+    );
+    return Change;
 }
