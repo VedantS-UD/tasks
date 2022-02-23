1e2270e10813c3de9d3475b0a5aebd77c5ff61dd
VedantS-UD <vsub@udel.edu>
2022-02-22 20:13:51 -0500

    Passed Integration Tests for Start Attempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,40 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempt, addAttempt] = useState<number>(4);
+    const [progress, inProgress] = useState<boolean>(false);
+    function nAttempt(): void {
+        addAttempt(attempt - 1);
+        inProgress(!progress);
+    }
+    const beginQuiz = (
+        <div>
+            {attempt}
+            <Button onClick={nAttempt} disabled={progress || attempt === 0}>
+                Start Quiz
+            </Button>
+        </div>
+    );
+    const endQuiz = (
+        <div>
+            <Button onClick={() => inProgress(!progress)} disabled={!progress}>
+                Stop Quiz
+            </Button>
+        </div>
+    );
+    const mull = (
+        <div>
+            <Button onClick={() => addAttempt(attempt + 1)} disabled={progress}>
+                Mulligan
+            </Button>
+        </div>
+    );
+    const Attempt = (
+        <div>
+            {beginQuiz}
+            {endQuiz}
+            {mull}
+        </div>
+    );
+    return Attempt;
 }
