1032bc7d819dbee1cf269bd425593da9e87e6ec6
VedantS-UD <vsub@udel.edu>
2022-02-26 14:05:12 -0500

    Cleaned Up Website (a little)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
-import { Container, Row, Col } from "react-bootstrap";
-import pic from "./IceCream.jpg";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
@@ -24,35 +21,8 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload (Edited by Vedant Subramanian).
             </p>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <p>Hello World!</p>
-            <h1>Ice Cream</h1>
-            <img
-                src={pic}
-                alt="A picture of Ice Cream"
-                width="300"
-                height="400"
-            />
-            <p>Ice Cream Flavors</p>
-            <ul>
-                <li>Chocolate</li>
-                <li>Vanilla</li>
-                <li>Strawberry</li>
-            </ul>
-            <Container>
-                <Row>
-                    <Col>
-                        <div id="rectangle"></div>
-                    </Col>
-                    <Col>
-                        <div id="rectangle"></div>
-                    </Col>
-                </Row>
-            </Container>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
