8e530eff776fd37872c902337598eb3762c5c62c
VedantS-UD <vsub@udel.edu>
2022-02-26 14:04:26 -0500

    Passed Integration Tests for Shove Box

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,20 +1,14 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-function ShoveBoxButton({
-    position,
-    setPosition
-}: {
-    position: number;
-    setPosition: (newPosition: number) => void;
-}) {
-    return (
-        <Button onClick={() => setPosition(4 + position)}>Shove the Box</Button>
-    );
+interface Box {
+    mBox: () => void;
+}
+function ShoveBoxButton({ mBox }: Box) {
+    return <Button onClick={mBox}>Shove the Box</Button>;
 }
 
-function MoveableBox(): JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+function MoveableBox({ position }: { position: number }): JSX.Element {
     return (
         <div
             data-testid="moveable-box"
@@ -32,19 +26,17 @@ function MoveableBox(): JSX.Element {
 }
 
 export function ShoveBox(): JSX.Element {
-    const box = MoveableBox();
-
+    const [position, setPosition] = useState<number>(10);
+    const box = MoveableBox({ position });
+    const move = () => setPosition(4 + position);
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}</span>
             <div>
-                <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
-                ></ShoveBoxButton>
+                <ShoveBoxButton mBox={move}></ShoveBoxButton>
                 {box}
-            </div> */}
+            </div>
         </div>
     );
 }
