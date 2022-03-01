4a6f1fee6d56e8772f85c51ccd3eb4d97f65ae9c
VedantS-UD <vsub@udel.edu>
2022-03-01 09:49:55 -0500

    Initial Attempt

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,57 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
+interface GiveProps {
+    giveAttempts: (newAtt: number) => void;
+    attempts: number;
+}
+interface UseProps {
+    att?: number;
+    cNumber: () => void;
+}
+
+function Use({ cNumber, att }: UseProps): JSX.Element {
+    return (
+        <Button onClick={cNumber} disabled={att === 0}>
+            use
+        </Button>
+    );
+}
+
+function Gain({ cNumber }: UseProps): JSX.Element {
+    return <Button onClick={cNumber}>gain</Button>;
+}
+
+function RequestAttempts({ attempts, giveAttempts }: GiveProps): JSX.Element {
+    function updateAtt(event: ChangeEvent) {
+        giveAttempts(parseInt(event.target.value) || 0);
+    }
+    return (
+        <div>
+            <Form.Group controlId="formAttempts">
+                <Form.Label>Attempts:</Form.Label>
+                <Form.Control value={attempts} onChange={updateAtt} />
+            </Form.Group>
+            <div>{attempts} attempts are being requested.</div>
+        </div>
+    );
+}
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, giveAttempts] = useState<number>(0);
+    const [att, setAttempt] = useState<number>(3);
+    const giveAtt = () => setAttempt(att + attempts);
+    const useAtt = () => setAttempt(att - 1);
     return (
         <div>
             <h3>Give Attempts</h3>
+            <RequestAttempts
+                attempts={attempts}
+                giveAttempts={giveAttempts}
+            ></RequestAttempts>
+            <Use att={att} cNumber={useAtt}></Use>
+            <Gain cNumber={giveAtt}></Gain>
+            <div>Currently, there are {att} attempts available.</div>
         </div>
     );
 }
